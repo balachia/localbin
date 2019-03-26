5fea99733c57214524bb1b96a742022976527917
Anthony Vashevko <avashevko@gmail.com>
2019-03-26 10:59:59 +0800

    echo -> printf

--- 16-colors.sh
+++ 16-colors.sh
@@ -2,26 +2,26 @@
 
 # color fg/default bg
 for i in {0..15}; do
-    [ $i == 8 ] && echo ''
-    echo "\033[38;5;${i}m::\c"
+    [ $i == 8 ] && printf '\n'
+    printf "\033[38;5;${i}m::"
 done
 
-echo '\033[0m\n'
+printf '\033[0m\n\n'
 
 # default fg/color bg
 for i in {0..15}; do
-    [ $i == 8 ] && echo ''
-    echo "\033[48;5;${i}m::\c"
+    [ $i == 8 ] && printf '\n'
+    printf "\033[48;5;${i}m::"
 done
 
-echo '\033[0m\n'
+printf '\033[0m\n\n'
 
 # swap fg/color bg
 for i in {0..15}; do
-    [ $i == 8 ] && echo ''
-    echo "\033[7;38;5;${i}m::\c"
+    [ $i == 8 ] && printf '\n'
+    printf "\033[7;38;5;${i}m::"
 done
 
 
 
-echo '\033[0m'
+printf '\033[0m'
